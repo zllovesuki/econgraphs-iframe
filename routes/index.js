d201d51952156e5351ec9cacd9f7863707facc2f
Rachel Chen <me@rachelchen.me>
2016-05-08 19:37:06 -0700

    legacy libraries

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,20 @@ var express = require('express');
 var router = express.Router();
 var request = require('superagent');
 var superagentPromisePlugin = require('superagent-promise-plugin');
-var cheerio = require('cheerio');
-var fs = require('fs');
 var Promise = require('bluebird');
+var cheerio = require('cheerio');
 var util = require('util');
+var fs = require('fs');
 
 superagentPromisePlugin.Promise = Promise;
 
-var bare = '<!DOCTYPE html><head><link href="/static/css/bootstrap.min.css" rel="stylesheet"><link href="/static/css/katex.min.css" rel="stylesheet"><link href="/static/css/econ-graphs-v2.css" rel="stylesheet"/></head><body>%s</body>';
+var bare = '<!DOCTYPE html><html><head>%s<link href="/static/css/bootstrap.min.css" rel="stylesheet"><link href="/static/css/katex.min.css" rel="stylesheet"></head><body>%s</body><html>';
+var legacyLib = '<script src="/static/js/archives/head.js"></script><link href="/static/css/style.css" rel="stylesheet" />';
+var modernLib = '<link href="/static/css/econ-graphs-v2.css" rel="stylesheet"/>';
+var legacy;
 
 router.get('/*', function(req, res, next) {
+	legacy = false;
 	request.get('http://www.econgraphs.org' + req.url)
 	.use(superagentPromisePlugin)
 	.then(function (re) {
@@ -19,13 +23,29 @@ router.get('/*', function(req, res, next) {
 		content = content.replace(/col-md-/g, 'col-xs-');
 		_$ = cheerio.load(content);
 		var body = _$('body').html();
-		body = util.format(bare, body);
+		if (content.indexOf('econGraphsApp') !== -1) {
+			legacy = true;
+		}
+
+		if (legacy) {
+			body = util.format(bare, legacyLib, body);
+		}else{
+			body = util.format(bare, modernLib, body);
+		}
+
 		$ = cheerio.load(body);
 		$('div').filter(function() {
 			return !!$(this).css('height');
 		}).remove();
 		$('body').find('nav').remove();
-		$('body').attr('ng-app', 'KineticGraphs');
+
+		if (legacy) {
+			$('html').attr('ng-app', 'econGraphsApp');
+			$('body').attr('ng-controller', 'Controller');
+		}else{
+			$('html').attr('ng-app', 'KineticGraphs');
+		}
+
 		res.send($.html());
 	})
 	.catch(function (err) {
