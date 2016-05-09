7dc882534739b220e85542d5d2b0e745fe1faa89
Rachel Chen <me@rachelchen.me>
2016-05-08 19:46:39 -0700

    edge

--- routes/index.js
+++ routes/index.js
@@ -12,10 +12,12 @@ superagentPromisePlugin.Promise = Promise;
 var bare = '<!DOCTYPE html><html><head>%s<link href="/static/css/bootstrap.min.css" rel="stylesheet"><link href="/static/css/katex.min.css" rel="stylesheet"></head><body>%s</body><html>';
 var legacyLib = '<script src="/static/js/archives/head.js"></script><link href="/static/css/style.css" rel="stylesheet" />';
 var modernLib = '<link href="/static/css/econ-graphs-v2.css" rel="stylesheet"/>';
-var legacy;
+var edgeLib = '<link href="/static/css/capm-style.css" rel="stylesheet"/><script src="/static/js/archives/kg-head.js"></script><script src="/static/js/archives/capm-v3.js"></script>';
+var legacy, edge;
 
 router.get('/*', function(req, res, next) {
 	legacy = false;
+	edge = false;
 	request.get('http://www.econgraphs.org' + req.url)
 	.use(superagentPromisePlugin)
 	.then(function (re) {
@@ -26,9 +28,14 @@ router.get('/*', function(req, res, next) {
 		if (content.indexOf('econGraphsApp') !== -1) {
 			legacy = true;
 		}
+		if (content.indexOf('MyApp')) {
+			edge = true;
+		}
 
 		if (legacy) {
 			body = util.format(bare, legacyLib, body);
+		}else if (edge) {
+			body = util.format(bare, edgeLib, body);
 		}else{
 			body = util.format(bare, modernLib, body);
 		}
@@ -42,6 +49,8 @@ router.get('/*', function(req, res, next) {
 		if (legacy) {
 			$('html').attr('ng-app', 'econGraphsApp');
 			$('body').attr('ng-controller', 'Controller');
+		}else if (edge) {
+			$('html').attr('ng-app', 'myApp');
 		}else{
 			$('html').attr('ng-app', 'KineticGraphs');
 		}
